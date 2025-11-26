13c5242a85b8632f7e481acbb3287bdc77a088a4
c0argpat <c0argpat@taniroda-hp-1>
2025-11-26 09:42:13 +0100

    crud végpontok

--- data/packages.js
+++ data/packages.js
@@ -2,6 +2,32 @@ import db from "./db.js";
 
 db.prepare(`CREATE TABLE IF NOT EXISTS packages (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    email TEXT UNIQUE,
-    password_hash TEXT
+    userId INTEGER,
+    tracking_number INTEGER UNIQUE AUTOINCREMENT,
+    sender_name TEXT,
+    sender_adress TEXT,
+    recipient_name TEXT,
+    recipient_address TEXT,
+    content TEXT,
+    weight INTEGER,
+    status TEXT,
+    status_history TEXT,
+    created_at DATETIME,
+    updated_at DATETIME
+    FOREIGN KEY (userId) REFERENCE users(id)
 )`).run();
+
+export const getPackageByUserID = (userId) => 
+    db.prepare("SELECT * FROM packages WHERE userId = ?").get(userId);
+
+export const newPackage = (sender_name, sender_adress, recipient_name, recipient_address, content, weight) => 
+    db.prepare("INSERT INTO packages (sender_name, sender_adress, recipient_name, recipient_address, content, weight ) VALUES (?, ?, ?)").run(sender_name, sender_adress, recipient_name, recipient_address, content, weight);
+
+export const getPackageByID = (id) => 
+    db.prepare("SELECT * FROM packages WHERE id = ?").get(id);
+
+export const updatePackageStatus = (status) => 
+    db.prepare("UPDATE packages SET status = ? WHERE id = ?").run(status, id);
+
+export const getPackageByTrackinNumber = (tracking_number) => 
+    db.prepare("SELECT * FROM packages WHERE tracking_number = ?").get(tracking_number);
